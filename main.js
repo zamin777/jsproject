4aca1c2bd6afa0d38fb027e0bdb84249b3556411
zamin777 <zamin.ehmedov.01@mail.ru>
2023-05-03 15:27:19 +0400

    first commit

--- main.js
+++ main.js
@@ -1,39 +1,42 @@
-const taskInput = document.getElementById('taskInput');
-const addTaskButton = document.getElementById('addTaskButton');
-const taskList = document.getElementById('taskList');
-const clearBtn = document.getElementById('clear')
-const sortBtn = document.getElementById('sortBtn')
-const sort = document.getElementById('sort')
+const taskInput = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("addTaskButton");
+const taskList = document.getElementById("taskList");
+const clearBtn = document.getElementById("clear");
+const sortBtn = document.getElementById("sortBtn");
+const sort = document.getElementById("sort");
 let flag = true;
 let ascendingOrder = true;
 sortBtn.addEventListener("click", () => {
   if (flag) {
-      sortImg.src = "./Group 38.svg";
-      console.log(sort.src)
-      flag = !flag
-  } else{
-      sortImg.src = "./Group 90.svg";
-      console.log(sort.src)
-      flag = !flag
+    sort.src = "./Group 38.svg";
+    console.log(sort.src);
+    flag = !flag;
+  } else {
+    sort.src = "./Group 90.svg";
+    console.log(sort.src);
+    flag = !flag;
   }
+});
 
-const items = Array.from(list.children).sort((a, b) => {
+const items = Array.from(taskList.children).sort((a, b) => {
   const aValue = a.firstElementChild.textContent.toLowerCase();
   const bValue = b.firstElementChild.textContent.toLowerCase();
 
   if (aValue < bValue) {
-      return ascendingOrder ? -1 : 1;
+    return ascendingOrder ? -1 : 1;
   } else if (aValue > bValue) {
-      return ascendingOrder ? 1 : -1;
+    return ascendingOrder ? 1 : -1;
   } else {
-      return 0;
+    return 0;
   }
 });
+
+
 function addTask(e) {
   e.preventDefault();
   const taskText = taskInput.value;
-  if (taskText.trim() !== '') {
-    const taskItem = document.createElement('li');
+  if (taskText.trim() !== "") {
+    const taskItem = document.createElement("li");
     taskItem.innerHTML = `
       <span class="task-text">${taskText}</span>
       <span class="task-actions">
@@ -42,26 +45,26 @@ function addTask(e) {
       </span>
     `;
     taskList.appendChild(taskItem);
-    taskInput.value = '';
+    taskInput.value = "";
   }
 }
 function completeTask(e) {
-  const taskItem = e.target.closest('li');
-  taskItem.classList.toggle('completed');
+  const taskItem = e.target.closest("li");
+  taskItem.classList.toggle("completed");
 }
 function deleteTask(e) {
-  const taskItem = e.target.closest('li');
+  const taskItem = e.target.closest("li");
   taskList.removeChild(taskItem);
 }
-addTaskButton.addEventListener('click', addTask);
-taskList.addEventListener('click', (e) => {
-  if (e.target.classList.contains('complete-button')) {
+addTaskButton.addEventListener("click", addTask);
+taskList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("complete-button")) {
     completeTask(e);
-  } else if (e.target.classList.contains('delete-button')) {
+  } else if (e.target.classList.contains("delete-button")) {
     deleteTask(e);
   }
 });
+
 clearBtn.addEventListener("click", () => {
   taskInput.value = "";
-  console.log(taskInput);
 });
